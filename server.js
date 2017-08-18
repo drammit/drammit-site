dfc69b5dacb4af58f8e21ee2ce08d1d141d3de77
Gaya Kessler <gaya.kessler@gmail.com>
2017-08-18 21:13:08 +0200

    setup server for site

--- server.js
+++ server.js
@@ -1,40 +1,37 @@
-var fs = require('fs');
-var koa = require('koa');
-var app = koa();
+const fs = require('fs');
+const Koa = require('koa');
+const app = new Koa();
+const serve = require('koa-static');
 
-var domain = process.env.PORT ? 'www.dramm.it' : 'localhost:3000';
+var domain = process.env.PORT ? 'www.dramm.it' : 'localhost';
 
-app.use(function *redirect301(next) {
-  if (this.req.headers.host !== domain) {
-    this.status = 301;
-    this.redirect(`https://${domain}`);
+app.use(async function redirect301(ctx) {
+  if (ctx.req.headers.host !== domain) {
+    ctx.status = 301;
+    ctx.redirect(`https://${domain}`);
   }
-
-  yield next;
 });
 
-app.use(require('koa-static')('docs', {
+app.use(serve(`${__dirname}/docs`, {
   gzip: true,
 }));
 
-app.use(function *pageNotFound(next) {
-  yield next;
+app.use(async function pageNotFound(ctx) {
+  if (ctx.status !== 404) return;
 
-  if (this.status !== 404) return;
+  ctx.status = 404;
 
-  this.status = 404;
-
-  switch (this.accepts('html', 'json')) {
+  switch (ctx.accepts('html', 'json')) {
     case 'html':
-      this.type = 'html';
-      this.body = '<html><head><title>Not found</title></head><body>Page Not Found</body></html>';
+      ctx.type = 'html';
+      ctx.body = 'Page Not Found';
       break;
     case 'json':
-      this.body = { message: 'Page Not Found' };
+      ctx.body = { message: 'Page Not Found' };
       break;
     default:
-      this.type = 'text';
-      this.body = 'Page Not Found';
+      ctx.type = 'text';
+      ctx.body = 'Page Not Found';
   }
 });
 
