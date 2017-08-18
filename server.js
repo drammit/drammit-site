cc50a8affdb3a064bb594b27e4a0ee6c0fcee14a
Gaya Kessler <gaya.kessler@gmail.com>
2017-08-18 21:32:25 +0200

    remove 301 redirect

--- server.js
+++ server.js
@@ -3,15 +3,6 @@ const Koa = require('koa');
 const app = new Koa();
 const serve = require('koa-static');
 
-var domain = process.env.PORT ? 'www.dramm.it' : 'localhost';
-
-// app.use(async function redirect301(ctx) {
-//   if (ctx.req.headers.host !== domain) {
-//     ctx.status = 301;
-//     ctx.redirect(`http://${domain}`);
-//   }
-// });
-
 app.use(serve(`${__dirname}/docs`, {
   gzip: true,
 }));
